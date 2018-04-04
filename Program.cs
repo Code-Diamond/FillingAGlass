7e351be0220439b071958ba918fef13965063b61
JaminROCK <KingJamin@hotmail.com>
2018-04-03 22:25:37 -0600

    Update Program.cs
    Added List<long>

--- Program.cs
+++ Program.cs
@@ -10,20 +10,20 @@ namespace FillingAGlass
     {
         static void Main(string[] args)
         {
-            //Create a Glass
+            //Create a glass
             List<int> glass = new List<int>();
             Console.WriteLine("The glass's current capacity is: " + glass.Capacity);
             Console.WriteLine("Adding water . . . ");
             bool overflows = false;
             //Pour water into it until it overflows
-            for(int water = 0; !overflows; water++)
+            for (int water = 0; !overflows; water++)
             {
                 //Try to add water
                 try
                 {
                     glass.Add(water);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     overflows = true;
                     Console.WriteLine("There were exactly {0} elements of water added to the glass until it overflowed.", water);
@@ -31,6 +31,30 @@ namespace FillingAGlass
             }
             Console.WriteLine("The glass's current capacity is: " + glass.Capacity);
             Console.WriteLine("The glass's element count is: " + glass.Count);
+
+
+
+            //Create a second glass
+            List<long> glass2 = new List<long>();
+            Console.WriteLine("The glass's current capacity is: " + glass2.Capacity);
+            Console.WriteLine("Adding water . . . ");
+            overflows = false;
+            //Pour water into it until it overflows
+            for (long water = 0; !overflows; water++)
+            {
+                //Try to add water
+                try
+                {
+                    glass2.Add(water);
+                }
+                catch (Exception e)
+                {
+                    overflows = true;
+                    Console.WriteLine("There were exactly {0} elements of water added to the glass until it overflowed.", water);
+                }
+            }
+            Console.WriteLine("The glass's current capacity is: " + glass2.Capacity);
+            Console.WriteLine("The glass's element count is: " + glass2.Count);
             Console.ReadLine();
         }
     }
